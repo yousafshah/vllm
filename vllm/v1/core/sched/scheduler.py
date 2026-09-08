fd61a35318a326578fc8f8cc98792dcdd67ef6e5
Matthew Bonanni <mbonanni@redhat.com>
2026-09-08 13:58:07 +0000

    Clean up HiSparse connector lifecycle
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -312,6 +312,7 @@ class Scheduler(SchedulerInterface):
             watermark=self.scheduler_config.watermark,
         )
         if self.kv_cache_config.hisparse_host_num_blocks is not None:
+            # TODO(NickLucche): unify scheduler-side connector initialization.
             hisparse_connector = find_hisparse_connector(self.connector)
             assert hisparse_connector is not None, (
                 "HiSparse host pool requires a configured HiSparseConnector"
